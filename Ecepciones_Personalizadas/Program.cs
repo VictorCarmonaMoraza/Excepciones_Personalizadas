7f5e3a13bca8f4f47b79990403b047785ea8f5aa
Victor Manuel Carmona Moraza <VictorCarmona9035@hotmail.com>
2022-03-28 21:31:50 +0200

    Excepciones Personalizadas

---
+++ Ecepciones_Personalizadas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ecepciones_Personalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] array = { };
+                int data = array[0];
+            }
+            catch (FormatException e)
+            {
+
+                String m = e.Message;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                //throw new ExcepcionPersonalizada("Se ha generado una excepcion personalizada");
+                throw new ExcepcionPersonalizada(e.ToString());
+                //String m = e.Message;
+            }
+            finally
+            {
+                Console.WriteLine("Hola a todos");
+            }
+        }
+    }
+}
